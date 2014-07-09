4b79a01bf36845d97af2d5dc13ae73ef5ffea94f
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-07-09 10:48:37 +0500

    fixed sign in redirections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,12 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      if current_user.user_type=="Consultant"
+      if current_user.user_type=="Consultant" 
         redirect_to templates_path
-      else
-        redirect_to user
+      elsif current_user.user_type=="Candidate"
+        redirect_to '/opening'
+      elsif current_user.user_type=="Manager"
+        redirect_to '/managerviewresumes'
       end
     else
       flash.now[:error] = 'Invalid email/password combination'
