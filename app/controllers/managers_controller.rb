96953d8ad3b7935d72fdd116b84f22c67054ac06
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-07-10 10:02:52 +0500

    fixed issue with saving selction of candidate for manager

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -12,16 +12,16 @@ class ManagersController < ApplicationController
       @mngr_comments = params[:managerviewresumes][:Comments]
       
       @selected=SelectionCandidate.where('"OpeningsID" = ? and "CandidateID" = ?',params[:opening_id],params[:candidate_id])
-      if @selected.nil?
+      if @selected.first.nil?
         @candidateSelection = SelectionCandidate.new(:ManagerID => current_user.id, :ManagerSelection => @mngr_selection, :Mngr_Tech_Skills => @mngr_tech_skills, :Mngr_Pre_Skills => @mngr_pre_skills, :Mngr_Mng_Skills => @mngr_mng_skills, :Mngr_Comments => @mngr_comments,:OpeningsID=> params[:opening_id], :CandidateID =>params[:candidate_id])
         @candidateSelection.save
       else
-       @selected.first.update_attribute(:ManagerID => current_user.id)
-       @selected.first.update_attribute(:ManagerSelection => @mngr_selection)
-       @selected.first.update_attribute(:Mngr_Tech_Skills => @mngr_tech_skills)
-       @selected.first.update_attribute(:Mngr_Pre_Skills => @mngr_pre_skills)
-       @selected.first.update_attribute(:Mngr_Mng_Skills => @mngr_mng_skills)
-       @selected.first.update_attribute(:Mngr_Comments => @mngr_comments)
+       @selected.first.update_attribute(:ManagerID, current_user.id)
+       @selected.first.update_attribute(:ManagerSelection, @mngr_selection)
+       @selected.first.update_attribute(:Mngr_Tech_Skills, @mngr_tech_skills)
+       @selected.first.update_attribute(:Mngr_Pre_Skills, @mngr_pre_skills)
+       @selected.first.update_attribute(:Mngr_Mng_Skills, @mngr_mng_skills)
+       @selected.first.update_attribute(:Mngr_Comments, @mngr_comments)
       end
     end
     #@condidateSelection = CondidateSelection.new(:UserID => current_user.id, :CandidateID => , :OpeningsID => , :ConsultantID => , :ConsultantSelection => , :Con_Tech_Skills => , :Con_Pre_Skills => , :Con_Mng_Skills => , :Con_Comments => , :ManagerID => , :ManagerSelection => mngr_selection, :Mngr_Tech_Skills => mngr_tech_skills, :Mngr_Pre_Skills => mngr_pre_skills, :Mngr_Mng_Skills => mngr_mng_skills, :Mngr_Comments => mngr_comments)
